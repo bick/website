d1d876c199516e1224f48d098137153b3f63f5a9
Work <owenbick@gmail.com>
2023-05-19 00:26:04 -0400

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,31 +1,31 @@
 import Link from "next/link";
 
 export default function Footer() {
-    return (
-        <footer className="footer">
-            <div className="container">
-                <ul className="footer__list">
-                    <li className="footer__list__item">
-                        <Link href="/" className="footer__list__link">
-                            Index
-                        </Link>
-                    </li>
-                    <li className="footer__list__item">
-                        <Link href="/about" className="footer__list__link">
-                            About
-                        </Link>
-                    </li>
-                    <li className="footer__list__item">
-                        <Link href="/contact" className="footer__list__link">
-                            Contact
-                        </Link>
-                    </li>
-                </ul>
-                <span>
-                    Copyright &copy; {(new Date().getFullYear())} Owen Bick. All Rights Reserved. This website
-                    doesn’t use any cookies.
-                </span>
-            </div>
-        </footer>
-    )
+  return (
+    <footer className="footer">
+      <div className="container">
+        <ul className="footer__list">
+          <li className="footer__list__item">
+            <Link href="/" className="footer__list__link">
+              Index
+            </Link>
+          </li>
+          <li className="footer__list__item">
+            <Link href="/about" className="footer__list__link">
+              About
+            </Link>
+          </li>
+          <li className="footer__list__item">
+            <Link href="/contact" className="footer__list__link">
+              Contact
+            </Link>
+          </li>
+        </ul>
+        <span>
+          Copyright &copy; {new Date().getFullYear()} Owen Bick. All Rights
+          Reserved. Made with ❤️ and ☕ in Boston.
+        </span>
+      </div>
+    </footer>
+  );
 }
