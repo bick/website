fc00fd152033ea2e954afffd5e5974cc2c6de247
Work <owenbick@gmail.com>
2023-07-11 14:33:19 -0400

    Update menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useState } from "react";
+
+const OffCanvasMenu = ({ isOpen, toggleOpen }) => {
+  return (
+    <div
+      className={`header__dropdown ${
+        isOpen ? "header__dropdown__open" : "header__dropdown__closed"
+      }`}
+    >
+      <ul>
+        <li className="md:hidden">
+          <Link href="/">Index</Link>
+        </li>
+        <li>
+          <Link href="/ratings">Ratings & Reviews</Link>
+        </li>
+        <li>
+          <Link
+            href="https://hourlytomonthly.com"
+            target="_blank"
+            rel="nofollow"
+          >
+            Hourly to Monthly
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="https://whois.weekendlabs.net"
+            target="_blank"
+            rel="nofollow"
+          >
+            WHOIS
+          </Link>
+        </li>
+        <li className="md:hidden">
+          <Link href="/about">About</Link>
+        </li>
+        <li className="md:hidden">
+          <Link href="/contact">Contact</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default function Header() {
   const router = useRouter();
   const currentRoute = router.pathname;
 
-  const [isActive, setActive] = useState(false);
+  const [isMenuOpen, setMenuOpen] = useState(false);
 
-  const toggleClass = () => {
-    setActive(!isActive);
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
   };
+
   return (
     <header className="header">
       <div className="header__logo">
@@ -22,47 +65,50 @@ export default function Header() {
       <ul className="header__list">
         <li className="header__item">
           <Link
+            href="/"
             className={
               currentRoute === "/" ? "header__link active" : "header__link"
             }
-            href="/"
           >
             Index
           </Link>
         </li>
         <li className="header__item">
           <Link
+            href="/about"
             className={
               currentRoute === "/about" ? "header__link active" : "header__link"
             }
-            href="/about"
           >
             About
           </Link>
         </li>
         <li className="header__item">
           <Link
+            href="/contact"
             className={
               currentRoute === "/contact"
                 ? "header__link active"
                 : "header__link"
             }
-            href="/contact"
           >
             Contact
           </Link>
         </li>
         <li className="header__item">
-          <div className={"header__dropdown"}>
-            <button className="header__link">More ↓</button>
-            <ul className="header__dropdown__list">
-              <Link href="/ratings">Ratings & Reviews</Link>
-              <Link href="https://hourlytomonthly.com" target="_blank" rel="nofollow">Hourly to Monthly</Link>
-              <Link href="https://whois.weekendlabs.net" target="_blank" rel="nofollow">WHOIS</Link>
-            </ul>
-          </div>
+          <button onClick={toggleMenu} className="header__button">
+            {isMenuOpen ? (
+              "← Close"
+            ) : (
+              <>
+                <span className="lg:hidden">Menu →</span>
+                <span className="hidden lg:inline">More →</span>
+              </>
+            )}
+          </button>
         </li>
       </ul>
+      <OffCanvasMenu isOpen={isMenuOpen} toggleOpen={toggleMenu} />
     </header>
   );
 }
