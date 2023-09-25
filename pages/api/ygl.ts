d419f1a25417a8e95ced9b5af3f392b6da711303
Owen Bick <owenbick@gmail.com>
2023-09-25 17:24:35 -0400

    Update ygl.ts

--- pages/api/ygl.ts
+++ pages/api/ygl.ts
@@ -3,14 +3,10 @@ import fs from "fs";
 import path from "path";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  try {
-    const filePath = path.join(process.cwd(), "static", "downloads", "ygl.xml");
-    const xmlContent = fs.readFileSync(filePath, "utf-8");
+  const filePath = path.join(process.cwd(), "static", "downloads", "ygl.xml");
+  const xmlContent = fs.readFileSync(filePath, "utf-8");
 
-    res.setHeader("Content-Type", "application/xml");
-    res.setHeader("Content-Disposition", "attachment; filename=ygl.xml");
-    res.send(xmlContent);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+  res.setHeader("Content-Type", "application/xml");
+  res.setHeader("Content-Disposition", "attachment; filename=ygl.xml"); // This header prompts the download
+  res.send(xmlContent);
 }
